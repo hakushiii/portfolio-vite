5cf2bcf9f9a7aac8d424f273ec920e987488c0e1
Jerome A <jeromealapad13@gmail.com>
2023-06-05 23:14:01 +0800

    Added Images

---
+++ src/views/HomePage.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h3>01001000 01001011 01010011 01001000</h3>
+        <h2>Jerome A.</h2>
+        <p>Computer Engineer / Junior Software Developer</p>
+        <img src="../../public/image/king.jpg" class="picture" alt="Profile Picture" />
+    </div>
+    <div>
+        <h3 class="section-head">Work</h3>
+            <p>Haven't worked yet, had university projects though.</p>
+        <h3 class="section-head">Bio</h3>
+            <p>2000</p>
+            <p>Spawned in the PH Server</p>
+            <p>2019 - 20XX</p>
+            <p>Started University taking Bachelor of Science in Computer Engineering</p>
+        <h3 class="section-head">SNS</h3>
+            <a href="https://www.linkedin.com/in/jerome-christian-alapad-3547631b7/">
+                <img class="logo" src="../../public/logo/linkedin-logo.svg" alt="LinkedIN" />
+            </a>
+            <a href="https://github.com/hakushiii">
+                <img class="logo" src="../../public/logo/github-logo.svg" alt="Github" />
+            </a>
+            <a href="https://twitter.com/hakushiii_">
+                <img class="logo" src="../../public/logo/twitter-logo.svg" alt="Twitter" />
+            </a>
+            
+    </div>
+    
+
+</template>
+
+<style scoped>
+.section-head {
+    color: #888
+}
+.picture {
+    height: 6em;
+    width: 6em;
+}
+.picture {
+    border-radius: 1.5ch;
+}
+.logo {
+    filter: invert(1);
+    height: 5em;
+    width: 5em;
+    margin-inline: 1em;
+}
+</style>
