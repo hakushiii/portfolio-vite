d85302abdafde816b4e72e0fdad6c52c2cadee07
Jerome A <jeromealapad13@gmail.com>
2023-06-05 12:58:48 +0800

    Startings of a HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h3>01001000 01001011 01010011 01001000</h3>
+        <h2>Jerome A.</h2>
+        <p>Computer Engineer / Junior Software Developer</p>
+        <img alt="profile picture" />
+    </div>
+    <div>
+        <h3 class="section-head">Work</h3>
+            <p>Haven't worked yet, had university projects though.</p>
+        <h3 class="section-head">Bio</h3>
+            <p>2000</p>
+            <p>Spawned in the PH Server</p>
+            <p>2019 - 20XX</p>
+            <p>Started University taking Bachelor of Science in Computer Engineering</p>
+        <h3 class="section-head">SNS</h3>
+            <img alt="inkedin" />
+            <img alt="github" />
+            <img alt="twitter" />
+    </div>
+    
+
+</template>
+
+<style scoped>
+.section-head {
+    color: #888
+    
+}
+</style>
